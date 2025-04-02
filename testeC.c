97d980e0ca26f20b7faa873660ce6ae80fd35cf6
LucasHoffmann2 <lucash.hoffmann23@gmail.com>
2025-04-02 09:34:58 -0300

    Update testeC.c

--- testeC.c
+++ testeC.c
@@ -2,13 +2,13 @@
 int main (){
 
    float hora, min, seg;
-
    printf("Digite hora, minuto, segundo: ");
    scanf("%f %f %f", &hora, &min, &seg);
 
    if (hora>1 && min>15){
  printf("segundos = %f\n", (hora*3600)+(min*60) +seg);
 }
+      
 else{
    printf("Quantidade de minutos: %f\n", (min) + (hora*60) +seg/60 );
 }
