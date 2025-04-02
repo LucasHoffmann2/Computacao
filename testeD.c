941019104d7adcfb7594b26a5b75bfc0c489e418
LucasHoffmann2 <lucash.hoffmann23@gmail.com>
2025-04-02 10:20:38 -0300

    Add files via upload

---
+++ testeD.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    float n1, n2, n3, media;
+
+    printf("Digite as tres notas: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+    media = (n1 + n2 +n3) / 3;
+
+    if (media >= 8)
+
+        printf("aprovado com distincao. ");
+    else if (media >=6)
+        printf("aprovado direto. ");
+    else if (media >=3)
+        printf("vai para a final");
+    else
+        printf("reprovado. ");
+
+    return 0;
+
+}
