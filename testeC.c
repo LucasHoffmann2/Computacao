c21a5bacfed50e5ab9655c0e04f8f12df3fd6f5c
LucasHoffmann2 <lucash.hoffmann23@gmail.com>
2025-04-02 09:37:44 -0300

    Add files via upload

--- testeC.c
+++ testeC.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
-int main (){
+int main ()
+{
 
-   float hora, min, seg;
-   printf("Digite hora, minuto, segundo: ");
-   scanf("%f %f %f", &hora, &min, &seg);
+    float hora, min, seg;
 
-   if (hora>1 && min>15){
- printf("segundos = %f\n", (hora*3600)+(min*60) +seg);
-}
-      
-else{
-   printf("Quantidade de minutos: %f\n", (min) + (hora*60) +seg/60 );
-}
+    printf("Digite hora, minuto, segundo: ");
+    scanf("%f %f %f", &hora, &min, &seg);
 
-   printf("Horario: %02f:%02f:%0f\n", hora, min, seg);
-   return 0;
+    if (hora>1 && min>15)
+    {
+        printf("segundos = %f\n", (hora *3600)+(min *60)+seg);
+    }
+    else
+    {
+        printf("Quantidade de minutos: %f\n", (min) + (hora*60)+seg/60 );
+    }
+
+    printf("Horario: %02f:%02f:%0f\n", hora, min, seg);
+    return 0;
 
 }
