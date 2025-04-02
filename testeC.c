1f724f7f3b7048f65354cdc37b56f7e47b992183
LucasHoffmann2 <lucash.hoffmann23@gmail.com>
2025-04-02 09:27:24 -0300

    Update testeC.c

--- testeC.c
+++ testeC.c
@@ -7,10 +7,10 @@ int main (){
    scanf("%f %f %f", &hora, &min, &seg);
 
    if (hora>1 && min>15){
- printf("segundos = %f\n", (hora *3600)+(min *60)+seg);
+ printf("segundos = %f\n", (hora*3600)+(min*60) +seg);
 }
 else{
-   printf("Quantidade de minutos: %f\n", (min) + (hora*60)+seg/60 );
+   printf("Quantidade de minutos: %f\n", (min) + (hora*60) +seg/60 );
 }
 
    printf("Horario: %02f:%02f:%0f\n", hora, min, seg);
