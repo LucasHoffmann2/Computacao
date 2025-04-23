9c8fecf77b481e891e77ed118e497382c988ce41
LucasHoffmann2 <lucash.hoffmann23@gmail.com>
2025-04-23 08:56:50 -0300

    Add files via upload

---
+++ ativiade.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero, num_inicio, num_fim, conta;
+
+    printf("Digite um numero inteiro para ser somado com os multiplos: ");
+    scanf ("%d", &numero);
+    printf("digite um numero para começar o intervalo: ");
+    scanf ("%d", &num_inicio);
+    printf("por ultimo, digite o numero para acabar o intervalo: ");
+    scanf("%d", &num_fim);
+
+    for(int i = num_inicio; i <= num_fim;)
+    {
+        if (numero * num_inicio > num_fim)
+        {
+            break;
+        }
+        conta = conta + numero + num_inicio;
+
+        num_inicio++;
+
+
+        printf("A soma dos múltiplos é: %d\n", conta);
+
+        return 0;
+    }
+
+}
