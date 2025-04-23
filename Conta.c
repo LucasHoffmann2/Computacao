5ae6efcb0a3b77e94b35a3f47f697e99b5708153
LucasHoffmann2 <lucash.hoffmann23@gmail.com>
2025-04-23 09:22:49 -0300

    Update Conta.c

--- Conta.c
+++ Conta.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int numero, num_inicio, num_fim, conta;
+    int numero, num_inicio, num_fim, soma = 0;
 
     printf("Digite um numero inteiro para ser somado com os multiplos: ");
     scanf ("%d", &numero);
@@ -11,18 +11,13 @@ int main()
     printf("por ultimo, digite o numero para acabar o intervalo: ");
     scanf("%d", &num_fim);
 
-    for(int i = num_inicio; i <= num_fim;)
+    for(int i = num_inicio; i <= num_fim; i++)
     {
         if (numero * num_inicio > num_fim)
         
-        conta = conta + numero + num_inicio;
-
-        num_inicio++;
-
-
-        printf("A soma dos múltiplos é: %d\n", conta);
+        soma = soma + i;
+    }
+        printf("A soma dos múltiplos é: %d\n", soma);
 
         return 0;
-    }
-
 }
