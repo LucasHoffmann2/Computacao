e5426e62c112e9ca3eb32a07a599094f34b3436c
LucasHoffmann2 <lucash.hoffmann23@gmail.com>
2025-05-07 08:57:34 -0300

    Add files via upload

---
+++ al.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int arredonda(float x)
+{
+    printf("escreva um numero: ");
+    if (x > 0)
+
+        return(x - (int) x>= 0.5) ? (int)x + 1 : (int)x;
+    else
+        return((int) x - x>= 0.5) ? (int)x - 1 : (int)x;
+
+    printf("%d\n", arredonda(1.4));
+    printf("%d\n", arredonda(1.5));
+    printf("%d\n", arredonda(-1.4));
+    printf("%d\n", arredonda(-1.5));
+
+    return 0;
+}
